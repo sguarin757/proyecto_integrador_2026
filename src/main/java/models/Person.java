2cbe79ec4612fcc9e1b01d6b4fc2ebc9b33d0057
Mi primer proyecto <sguarin757@gmail.com>
2026-02-27 22:22:35 -0500

    modelo person

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,71 @@
+package models;
+
+public class Person {
+    Long userId;
+    String code;
+    String documentNumber;
+    String firstName;
+    String lastName;
+    Boolean status;
+
+    public Person() {
+
+    }
+
+    public Person(Long userId, String code, String documentNumber, String firstName, String lastName, Boolean status) {
+        this.userId = userId;
+        this.code = code;
+        this.documentNumber = documentNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.status = status;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
